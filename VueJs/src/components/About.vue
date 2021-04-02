9bea6e2a126e2078499191d6dd0750f4f9c64c71
srabosthy <srabosthydas@gmail.com>
2021-04-03 00:37:05 +0600

    vuejs added

---
+++ VueJs/src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>{{ title }}</h3>
+        <h5>{{ text }}</h5>
+    </div>
+</template>
+<script>
+export default {
+   data(){
+       return{
+           title: 'About Us',
+           text: 'This is our about page'
+       }
+   }
+}
+</script>
+<style scoped>
+
+</style>
